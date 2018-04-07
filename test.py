7491445c987118a4b378972f0ab20cfccdbdc6ec
vama <vamagithub@gmail.com>
2018-04-07 17:00:07 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+import Expression
+
+ink = 4
+act = 1
+tig = 3.2
+Expression.evaluation(int(ink*act/2+3/2*act+2+tig))
+
+qui=4
+add =2
+god=2
+Expression.evaluation(int(qui*add/4-6/2+2/3*6/god))
+
+a=4
+g=3
+Expression.evaluation(int(1/3*a/4-6/2+2/3*6/g))
+
+a=b=c=q=r=m=1
+equation = (8.8*(a+b)*2/c-0.5+2*a/(q+r))/(a+b)*(1/m)
+Expression.evaluation(equation)
+
+
+Expression.evaluation((-b+(b*b)+2*4*a*c)/2*a)
+
+x=y=1
+Expression.evaluation((7.7*b*(x*y+a)/c-0.8+2*b)/(x+a)*(1/y))
